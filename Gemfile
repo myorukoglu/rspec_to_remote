034382ef1e1ad937551c00360eb1219d2583142c
Murat Yorukoglu <murat@breakthrough.com>
2015-10-05 09:24:02 -0700

    update testings

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'rake'
-gem 'rspec', '~> 3.1.0'
-gem 'capybara', '~> 2.4.4'
-gem 'selenium-webdriver', '2.44.0'  # capybara no longer seems to be including this
+gem 'rspec', '~> 3.3.0'
+gem 'capybara', '~> 2.5.0'
+gem 'selenium-webdriver', '2.47.1'
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,20 +1,24 @@
 require './spec/spec_helper'
-feature "Signing in to Shopwell" do
+feature "Signing in to ArtWeb" do
 
   it 'should let me in with a valid account' do
-    visit '/'
-    click_link 'Sign In'
-    fill_in 'j_username', :with => 'bigmac@gmail.com'
-    fill_in 'j_password', :with => 'password123'
-    click_button 'Sign In'
-    expect(page).to have_content("Find foods you'll love.")
+    visit 'users/sign_in'
+
+    fill_in 'user[email]', :with => 'Murat@breakthrough.com'
+    fill_in 'user[password]', :with => 'asdfasdf'
+    
+    click_button "Log in"
+    expect(page).to have_content("Artist of the Month")
   end
 
   it 'should not let me in with an invalid account' do
-    visit '/'
-    click_link 'Sign In'
-    click_button 'Sign In'
-    expect(page).to have_content("We're unable to sign you in. Please try again.")
+    visit 'users/sign_in'
+
+    fill_in 'user[email]', :with => 'a@a.com'
+    fill_in 'user[password]', :with => 'asdfasdf'
+
+    click_button "Log in"
+    expect(page).to have_content("Invalid email or password")
   end
 
 end
