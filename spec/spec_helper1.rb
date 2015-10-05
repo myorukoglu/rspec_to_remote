3e54f77233a771ec584a05bcfee33f8e3941eec8
Murat Yorukoglu <murat@breakthrough.com>
2015-10-05 09:29:29 -0700

    remove old files

--- spec/spec_helper1.rb
+++
@@ -1,7 +0,0 @@
-require 'capybara/rspec'
-require 'rubygems'
-require 'bundler/setup'
-
-Capybara.default_driver = :selenium
-Capybara.app_host = 'http://192.168.15.214:8080/'
-Capybara.run_server = false
