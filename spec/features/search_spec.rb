3dd0ddd6c801365041bfbe9d51ba44c364d95416
Murat Yorukoglu <murat@breakthrough.com>
2015-12-07 15:29:37 -0500

    update to mdlive

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,24 +1,41 @@
 require './spec/spec_helper'
-feature "Signing in to ArtWeb" do
+feature "Sign in page for MDLIVE" do
 
-  it 'should let me in with a valid account' do
-    visit 'users/sign_in'
+  context "A valid account" do
 
-    fill_in 'user[email]', :with => 'Murat@breakthrough.com'
-    fill_in 'user[password]', :with => 'asdfasdf'
-    
-    click_button "Log in"
-    expect(page).to have_content("Artist of the Month")
+    it 'should be allowed to login' do
+      visit ''
+
+      find("a.showlogin").click
+
+      fill_in 'login[email]', :with => 'sif123'
+      fill_in 'login[password]', :with => 'mdlive789'
+
+      click_button "Sign In"
+      expect(page).to have_content("My Health")
+    end
+
+    it 'should have the correct content on the dashboard' do
+      visit ''
+
+      find("a.showlogin").click
+
+      fill_in 'login[email]', :with => 'sif123'
+      fill_in 'login[password]', :with => 'mdlive789'
+
+      click_button "Sign In"
+      expect(page).to have_content("See a Doctor")
+      expect(page).to have_content("your Health")
+    end
   end
-
-  it 'should not let me in with an invalid account' do
-    visit 'users/sign_in'
-
-    fill_in 'user[email]', :with => 'a@a.com'
-    fill_in 'user[password]', :with => 'asdfasdf'
-
-    click_button "Log in"
-    expect(page).to have_content("Invalid email or password")
-  end
+  # it 'should not let me in with an invalid account' do
+  #   visit 'users/sign_in'
+  #
+  #   fill_in 'user[email]', :with => 'a@a.com'
+  #   fill_in 'user[password]', :with => 'asdfasdf'
+  #
+  #   click_button "Log in"
+  #   expect(page).to have_content("Invalid email or password")
+  # end
 
 end
